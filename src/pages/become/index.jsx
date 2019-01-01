84f4ab850f35203072dd2041ac7db0e4483ac301
ridho muhammad <ulunmuhammadridho@gmail.com>
2019-01-01 17:08:17 +0700

    master add nested routing

---
+++ src/pages/become/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Become extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Become</h1>
+      </div>
+    );
+  }
+}
+
+export default Become;
---
+++ src/pages/become/member.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Member extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>ssss</p>
+      </div>
+    );
+  }
+}
+
+export default Member;
